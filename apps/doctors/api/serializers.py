d24963298075baf1360e87c54df0daf928d11fb3
sorena <mdn1376@gmail.com>
2024-08-06 13:24:26 +0330

    fix: fix error in GetDoctorTicketSerializer with create new instance of DoctorAdmission & some little changes

--- apps/doctors/api/serializers.py
+++ apps/doctors/api/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from apps.doctors.models import DoctorAdmission, Doctor
@@ -7,9 +8,6 @@ class GetDoctorTicketSerializer(serializers.Serializer):
     patient_id = serializers.IntegerField()
 
     def validate_patient_id(self, value):
-        doctor_admission = DoctorAdmission.objects.filter(patient_id=value)
-        if doctor_admission:
-            raise serializers.ValidationError({"error": "you already reserved for this doctor"})
         if not isinstance(value, int):
             raise serializers.ValidationError({"error": "patient_id must be an integer."})
         return value
@@ -22,14 +20,18 @@ class GetDoctorTicketSerializer(serializers.Serializer):
             raise serializers.ValidationError({"error": "Doctor is not available."})
 
         patient_id = validated_data.get("patient_id")
+        with transaction.atomic():
+            # Check if an admission with this patient_id already exists for the doctor
+            if DoctorAdmission.objects.filter(doctor=doctor, patient_id=patient_id).exists():
+                raise serializers.ValidationError({"error": "You already reserved for this doctor."})
 
-        current_admissions_count = DoctorAdmission.objects.filter(doctor=doctor).count()
+            current_admissions_count = DoctorAdmission.objects.filter(doctor=doctor).count()
 
-        if current_admissions_count < doctor.admission_count:
-            DoctorAdmission.objects.create(
-                doctor=doctor,
-                patient_id=patient_id
-            )
-        else:
-            raise serializers.ValidationError({"error": "doctor has no remaining admissions."})
-        return validated_data
+            if current_admissions_count < doctor.admission_count:
+                admission = DoctorAdmission.objects.create(
+                    doctor=doctor,
+                    patient_id=patient_id
+                )
+                return admission
+            else:
+                raise serializers.ValidationError({"error": "doctor has no remaining admissions."})
